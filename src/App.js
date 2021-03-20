7e2bfce7d33c8975764a15a3f0f68c2634f6169e
muhammadtayyabgujjar <gujjartayyab9494@gmail.com>
2021-03-20 09:02:38 +0500

    serverless

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './App.css'
 import {Display} from './component/display'
 import {Setdata} from './component/settodo'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App = ( ) =>{
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+      fetch(`/.netlify/functions/hello?name=from Serverless Function tayyab`)
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
     return(
         <div className="App">
+             {data.message}
             <h1 style={{fontFamily: "serif"}}> TODO 
             <br></br>
             APPLICATION</h1>
